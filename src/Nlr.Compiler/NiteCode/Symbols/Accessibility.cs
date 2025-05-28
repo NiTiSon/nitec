337e3a041fb62048d8e09f814871d36f80824ce0
NiTiSon <NiTiSon@users.noreply.github.com>
2025-05-28 20:25:45 +0300

    Accessibility.cs

---
+++ src/Nlr.Compiler/NiteCode/Symbols/Accessibility.cs
@@ -0,0 +1,37 @@
+namespace Nlr.Compiler.NiteCode.Symbols;
+
+/// <summary>
+/// Defines the accessibility levels for symbols in the code.
+/// </summary>
+public enum Accessibility
+{
+	/// <summary>
+	/// Accessible only within the container.
+	/// </summary>
+	Private = 0,
+
+	/// <summary>
+	/// Threat as <see cref="Protected"/> within same package, otherwise as <see cref="Private"/>.
+	/// </summary>
+	Family = 1,
+
+	/// <summary>
+	/// Threat as <see cref="Public"/> within same package, otherwise as <see cref="Private"/>.
+	/// </summary>
+	Internal = 2,
+
+	/// <summary>
+	/// Accessible from the container and all derived types.
+	/// </summary>
+	Protected = 3,
+
+	/// <summary>
+	/// Threat as <see cref="Public"/> within same package, otherwise as <see cref="Protected"/>.
+	/// </summary>
+	Friend = 4,
+
+	/// <summary>
+	/// Always accessible.
+	/// </summary>
+	Public = 5,
+}
