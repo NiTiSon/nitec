05086b13b99d1de403a7cd1a4d0d0d9b478de99e
NiTiSon <NiTiSon@users.noreply.github.com>
2025-05-30 12:51:16 +0300

    test project

---
+++ src/Nlr.Tests/CodeBase.cs
@@ -0,0 +1,12 @@
+﻿namespace Nlr.Tests;
+
+internal static class CodeBase
+{
+	public const string ExampleAddFunction = """
+		module something;
+		
+		public add(x: i32, y: i32) -> i32 {
+			return x + y;
+		}
+		""";
+}
