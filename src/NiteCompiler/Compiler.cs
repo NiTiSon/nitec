3133fae7f3ce1e3c1c51021df96dc7aec5c2bfa2
NiTiSon <NiTiSon@users.noreply.github.com>
2025-08-19 13:57:41 +0300

    ф

---
+++ src/NiteCompiler/Compiler.cs
@@ -0,0 +1,60 @@
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using NiteCompiler.CodeAnalysis.Symbols;
+using NiteCompiler.CodeAnalysis.Syntax;
+using NiteCompiler.CodeAnalysis.Text;
+using NiTiS.Compiler.Diagnostics;
+
+namespace NiteCompiler;
+
+public class Compiler
+{
+    private readonly PackageSymbol _package;
+    private SymbolTable _symbolTable;
+
+    private int _atom = 0;
+
+    public Compiler(string packageName)
+    {
+        _package = new PackageSymbol(packageName);
+        _symbolTable = new SymbolTable();
+    }
+
+    public void Compile(ReadOnlySpan<FileInfo> files, DiagnosticBag diagnostics, Stream output)
+    {
+        if (Interlocked.Exchange(ref _atom, 1) == 0)
+        {
+            try
+            {
+                foreach (var file in files)
+                {
+                    Console.WriteLine($"Compiling {file.FullName}");
+                    NiteLexer lexer = new(new StringText(File.ReadAllText(file.FullName)));
+
+                    Token token;
+                    do
+                    {
+                        token = lexer.Lex();
+                        Console.WriteLine(token.ToString());
+                    } while (token.Kind != SyntaxKind.EndOfFile);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new OperationCanceledException(
+                    "Internal compilation exception. Please report this problem to compiler developers!!!",
+                    ex);
+            }
+            finally
+            {
+                Interlocked.Exchange(ref _atom, 0);
+            }
+        }
+        else
+        {
+            throw new InvalidOperationException("Compilation process is already running.");
+        }
+    }
+}
