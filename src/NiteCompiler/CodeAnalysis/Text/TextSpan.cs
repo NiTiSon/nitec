bf9f22ef06c68fd9d3aabf15464d1a2369952c9e
NiTiSon <NiTiSon@users.noreply.github.com>
2025-10-29 18:49:14 +0300

    fix span

--- src/NiteCompiler/CodeAnalysis/Text/TextSpan.cs
+++ src/NiteCompiler/CodeAnalysis/Text/TextSpan.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.Serialization;
+using CommunityToolkit.Diagnostics;
 
 namespace NiteCompiler.CodeAnalysis.Text;
 
@@ -14,7 +15,7 @@ public readonly struct TextSpan
 	public TextSpan(int start, int length)
 	{
         ArgumentOutOfRangeException.ThrowIfNegative(start);
-        ArgumentOutOfRangeException.ThrowIfGreaterThan(start, start + length);
+        ArgumentOutOfRangeException.ThrowIfNegative(length);
 
         Start = start;
 		Length = length;
@@ -27,7 +28,7 @@ public readonly struct TextSpan
 
 	public bool Contains(TextSpan span)
 	{
-		return span.Start >= Start && span.End <= this.End;
+		return span.Start >= Start && span.End <= End;
 	}
 
 	public bool OverlapsWith(TextSpan span)
@@ -41,11 +42,11 @@ public readonly struct TextSpan
 	public TextSpan? Overlap(TextSpan span)
 	{
 		int overlapStart = Math.Max(Start, span.Start);
-		int overlapEnd = Math.Min(this.End, span.End);
+		int overlapEnd = Math.Min(End, span.End);
 
 		return overlapStart < overlapEnd
-			? TextSpan.FromBounds(overlapStart, overlapEnd)
-			: (TextSpan?)null;
+			? FromBounds(overlapStart, overlapEnd)
+			: null;
 	}
 
 	public bool IntersectsWith(TextSpan span)
@@ -61,11 +62,38 @@ public readonly struct TextSpan
 	public TextSpan? Intersection(TextSpan span)
 	{
 		int intersectStart = int.Max(Start, span.Start);
-		int intersectEnd = int.Min(this.End, span.End);
+		int intersectEnd = int.Min(End, span.End);
 
 		return intersectStart <= intersectEnd
-			? TextSpan.FromBounds(intersectStart, intersectEnd)
-			: (TextSpan?)null;
+			? FromBounds(intersectStart, intersectEnd)
+			: null;
+	}
+
+	public TextSpan First(int length) => SubSpan(0, length);
+
+	public TextSpan Last(int length)
+	{
+		if (length > Length)
+		{
+			ThrowHelper.ThrowArgumentOutOfRangeException(nameof(length));
+		}
+
+		return SubSpan(Length - length, length);
+	}
+
+	public TextSpan SubSpan(int position, int length)
+	{
+		if ((uint)position > (uint)Length)
+		{
+			ThrowHelper.ThrowArgumentOutOfRangeException(nameof(position));
+		}
+
+		if ((uint)length > (uint)(Length - position))
+		{
+			ThrowHelper.ThrowArgumentOutOfRangeException(nameof(length));
+		}
+
+		return new(Start + position, length);
 	}
 
 	public SourceSpan Contextualize(SourceText source)
