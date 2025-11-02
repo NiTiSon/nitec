176dfda9fcf2be469c85839702bc0f3992095a68
NiTiSon <NiTiSon@users.noreply.github.com>
2025-11-03 00:10:25 +0300

    function usages

--- src/NiteCompiler/CodeAnalysis/Symbols/Source/SourceFunctionSymbol.cs
+++ src/NiteCompiler/CodeAnalysis/Symbols/Source/SourceFunctionSymbol.cs
@@ -1,10 +1,13 @@
+using System.Collections.Generic;
 using NiteCompiler.CodeAnalysis.Syntax;
 
 namespace NiteCompiler.CodeAnalysis.Symbols.Source;
 
 internal sealed class SourceFunctionSymbol : FunctionSymbol
 {
-	internal SourceFunctionSymbol(IContainerSymbol containingSymbol, string name, FunctionDeclarationSyntax syntax)
+	public List<UseOrUseAsDirectiveSyntax> Usages { get; } = [];
+
+	public SourceFunctionSymbol(IContainerSymbol containingSymbol, string name, FunctionDeclarationSyntax syntax)
 		: base(containingSymbol, name, syntax)
 	{}
 }
