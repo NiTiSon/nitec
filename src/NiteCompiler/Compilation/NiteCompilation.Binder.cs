af4a573b34d2c6c7162eae1dc92f36f0bfe8b01e
NiTiSon <NiTiSon@users.noreply.github.com>
2026-04-03 12:34:32 +0300

    partitial NiteCompilation

---
+++ src/NiteCompiler/Compilation/NiteCompilation.Binder.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using NiteCompiler.CodeAnalysis.Binding;
+using NiteCompiler.CodeAnalysis.Syntax;
+
+namespace NiteCompiler.Compilation;
+
+public sealed partial class NiteCompilation
+{
+	private WeakReference<BinderFactory>?[]? _binderFactories;
+
+	internal BinderFactory GetBinderFactory(SyntaxTree syntaxTree)
+	{
+		return GetBinderFactory(syntaxTree, ref _binderFactories);
+	}
+
+	private BinderFactory GetBinderFactory(SyntaxTree syntaxTree,
+		ref WeakReference<BinderFactory>?[]? cachedBinderFactories)
+	{
+		var treeNum = GetSyntaxTreeOrdinal(syntaxTree);
+		WeakReference<BinderFactory>?[]? binderFactories = cachedBinderFactories;
+		if (binderFactories == null)
+		{
+			binderFactories = new WeakReference<BinderFactory>[this.SyntaxTrees.Length];
+			binderFactories = Interlocked.CompareExchange(ref cachedBinderFactories, binderFactories, null) ??
+			                  binderFactories;
+		}
+
+		var previousWeakReference = binderFactories[treeNum];
+		if (previousWeakReference != null && previousWeakReference.TryGetTarget(out BinderFactory? previousFactory))
+		{
+			return previousFactory;
+		}
+
+		return AddNewFactory(syntaxTree, ref binderFactories[treeNum]);
+	}
+
+	private BinderFactory AddNewFactory(SyntaxTree syntaxTree, [NotNull] ref WeakReference<BinderFactory>? slot)
+	{
+		var newFactory = new BinderFactory(this, syntaxTree);
+		var newWeakReference = new WeakReference<BinderFactory>(newFactory);
+
+		while (true)
+		{
+			WeakReference<BinderFactory>? previousWeakReference = slot;
+			if (previousWeakReference != null && previousWeakReference.TryGetTarget(out BinderFactory? previousFactory))
+			{
+				Debug.Assert(slot != null);
+				return previousFactory;
+			}
+
+			if (Interlocked.CompareExchange(ref slot!, newWeakReference, previousWeakReference) ==
+			    previousWeakReference)
+			{
+				return newFactory;
+			}
+		}
+	}
+
+	internal Binder GetBinder(SyntaxNode node)
+	{
+		return GetBinderFactory(node.Tree).GetBinder(node);
+	}
+}
--- src/NiteCompiler/Compilation/NiteCompilation.cs
+++ src/NiteCompiler/Compilation/NiteCompilation.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.ComponentModel;
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Threading;
 using LLVMSharp.Interop;
-using NiteCompiler.CodeAnalysis.Binding;
 using NiteCompiler.CodeAnalysis.Declarations;
 using NiteCompiler.CodeAnalysis.Symbols;
 using NiteCompiler.CodeAnalysis.Symbols.Source;
@@ -19,7 +17,7 @@ using static LLVMSharp.Interop.LLVM;
 
 namespace NiteCompiler.Compilation;
 
-public sealed class NiteCompilation
+public sealed partial class NiteCompilation
 {
 	private const string FallbackLibraryName = "unnamed-library";
 
@@ -104,58 +102,6 @@ public sealed class NiteCompilation
 		}
 
 		throw new KeyNotFoundException($"SyntaxTree is not used within this compilation: '{tree.Filename ?? "empty-filepath"}'");
-	}
-
-	private WeakReference<BinderFactory>?[]? _binderFactories;
-
-	internal BinderFactory GetBinderFactory(SyntaxTree syntaxTree)
-	{
-		return GetBinderFactory(syntaxTree, ref _binderFactories);
-	}
-
-	private BinderFactory GetBinderFactory(SyntaxTree syntaxTree, ref WeakReference<BinderFactory>?[]? cachedBinderFactories)
-	{
-		var treeNum = GetSyntaxTreeOrdinal(syntaxTree);
-		WeakReference<BinderFactory>?[]? binderFactories = cachedBinderFactories;
-		if (binderFactories == null)
-		{
-			binderFactories = new WeakReference<BinderFactory>[this.SyntaxTrees.Length];
-			binderFactories = Interlocked.CompareExchange(ref cachedBinderFactories, binderFactories, null) ?? binderFactories;
-		}
-
-		var previousWeakReference = binderFactories[treeNum];
-		if (previousWeakReference != null && previousWeakReference.TryGetTarget(out BinderFactory? previousFactory))
-		{
-			return previousFactory;
-		}
-
-		return AddNewFactory(syntaxTree, ref binderFactories[treeNum]);
-	}
-
-	private BinderFactory AddNewFactory(SyntaxTree syntaxTree, [NotNull] ref WeakReference<BinderFactory>? slot)
-	{
-		var newFactory = new BinderFactory(this, syntaxTree);
-		var newWeakReference = new WeakReference<BinderFactory>(newFactory);
-
-		while (true)
-		{
-			WeakReference<BinderFactory>? previousWeakReference = slot;
-			if (previousWeakReference != null && previousWeakReference.TryGetTarget(out BinderFactory? previousFactory))
-			{
-				Debug.Assert(slot != null);
-				return previousFactory;
-			}
-
-			if (Interlocked.CompareExchange(ref slot!, newWeakReference, previousWeakReference) == previousWeakReference)
-			{
-				return newFactory;
-			}
-		}
-	}
-
-	internal Binder GetBinder(SyntaxNode node)
-	{
-		return GetBinderFactory(node.Tree).GetBinder(node);
 	}
 
 	internal bool LookingForSpecialTypes
