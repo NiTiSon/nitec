72a25b6415653a9f3fb882ccff404e54070720be
NiTiSon <NiTiSon@users.noreply.github.com>
2026-04-25 14:40:11 +0300

    drop instruction

---
+++ src/NiteCompiler/IntermediateRepresentation/DropInstruction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using NiteCompiler.IntermediateRepresentation.Ssa;
+
+namespace NiteCompiler.IntermediateRepresentation;
+
+internal sealed class DropInstruction(SsaValue value) : Instruction
+{
+	public SsaValue Value { get; } = value;
+	public override bool IsBranch => false;
+
+	public override void Emit(BinaryWriter writer)
+	{
+		throw new NotImplementedException();
+	}
+
+	public override void Write(TextWriter writer)
+	{
+		writer.Write("drop ");
+		Value.Write(writer);
+	}
+}
