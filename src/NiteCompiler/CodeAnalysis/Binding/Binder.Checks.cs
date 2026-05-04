1ef8e7b14c06e9e8eff1305f10de9a99b6fc777e
NiTiSon <NiTiSon@users.noreply.github.com>
2026-05-04 15:05:03 +0300

    checks

---
+++ src/NiteCompiler/CodeAnalysis/Binding/Binder.Checks.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NiteCompiler.Diagnostics;
+
+namespace NiteCompiler.CodeAnalysis.Binding;
+
+internal partial class Binder
+{
+	private const int ValueKindInsignificantBits = 2;
+	private const BindValueKind ValueKindSignificantBitsMask = unchecked((BindValueKind)~((1 << ValueKindInsignificantBits) - 1));
+
+	[Flags]
+	internal enum BindValueKind : ushort
+	{
+		RValue = 1 << ValueKindInsignificantBits,
+
+		LValue = 2 << ValueKindInsignificantBits,
+
+		RefersToLocation = 4 << ValueKindInsignificantBits,
+	}
+
+	private BoundExpression CheckValue(BoundExpression expression, BindValueKind valueKind, BindingDiagnosticBag diagnostics)
+	{
+		BindValueKind actual = expression.ValueKind;
+
+		if ((actual & ValueKindSignificantBitsMask) == (valueKind & ValueKindSignificantBitsMask))
+			return expression;
+
+		// TODO: Error in diagnostic
+
+		return expression;
+	}
+}
--- src/NiteCompiler/CodeAnalysis/Binding/Binder.Expressions.cs
+++ src/NiteCompiler/CodeAnalysis/Binding/Binder.Expressions.cs
@@ -12,19 +12,6 @@ namespace NiteCompiler.CodeAnalysis.Binding;
 
 internal partial class Binder
 {
-	private const int ValueKindInsignificantBits = 2;
-	private const BindValueKind ValueKindSignificantBitsMask = unchecked((BindValueKind)~((1 << ValueKindInsignificantBits) - 1));
-
-	[Flags]
-	internal enum BindValueKind : ushort
-	{
-		RValue = 1 << ValueKindInsignificantBits,
-
-		LValue = 2 << ValueKindInsignificantBits,
-
-		Variable = 4 << ValueKindInsignificantBits,
-	}
-
 	private BoundBadExpression BadExpression(SyntaxNode syntax)
 	{
 		return BadExpression(syntax, LookupResultKind.Empty, []);
@@ -134,18 +121,6 @@ internal partial class Binder
 		}
 
 		throw new NotImplementedException("Operators && and || not implemented yet.");
-	}
-
-	private BoundExpression CheckValue(BoundExpression expression, BindValueKind valueKind, BindingDiagnosticBag diagnostics)
-	{
-		var actual = expression.ValueKind;
-
-		if ((actual & ValueKindSignificantBitsMask) == (valueKind & ValueKindSignificantBitsMask))
-			return expression;
-
-		// TODO: Error in diagnostic
-
-		return expression;
 	}
 
 	private BoundExpression BindValue(ExpressionSyntax syntax, BindingDiagnosticBag diagnostics, BindValueKind valueKind)
