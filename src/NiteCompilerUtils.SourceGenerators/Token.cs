0b9e84bf6e3606435f987d7046a4eced671ab9ab
NiTiSon <NiTiSon@users.noreply.github.com>
2026-07-02 23:35:29 +0300

    add source generator files (for future, or not)

---
+++ src/NiteCompilerUtils.SourceGenerators/Token.cs
@@ -0,0 +1,9 @@
+﻿namespace NiteCompilerUtils.SourceGenerators;
+
+public record struct Token
+{
+	public string Name;
+	public string? Text;
+	public string? Binary;
+	public string? Unary;
+}
---
+++ src/NiteCompilerUtils.SourceGenerators/TokenGenerator.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace NiteCompilerUtils.SourceGenerators;
+
+[Generator]
+public sealed class TokenGenerator : IIncrementalGenerator
+{
+	public void Initialize(IncrementalGeneratorInitializationContext context)
+	{
+	}
+}
